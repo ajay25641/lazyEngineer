4e0cfbf3f99f46edc82dfb6245cd2fdd56281e60
ajay25641 <ajay25641@gmail.com>
2022-09-28 10:10:10 +0530

    third

--- main.route.js
+++ main.route.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/',(req,res)=>{
-    res.send('welcome to new');
+    res.send('welcome to new site login');
 });
 
 app.use(PATH.ALERT, alertRoute);
