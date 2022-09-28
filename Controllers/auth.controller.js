2f27125fc5531fe9cf8a90e0019a536a782b1ab2
ajay25641 <ajay25641@gmail.com>
2022-09-28 17:39:34 +0530

    g

--- Controllers/auth.controller.js
+++ Controllers/auth.controller.js
@@ -2,42 +2,8 @@ const userModel = require("../Models/user");
 const JWTService = require("../CommonLib/JWTtoken");
 const encryptDecrypt = require("../CommonLib/encryption-decryption");
 const tokenModel = require("../Models/token");
-const response = require("../response");
 
-/*
-async function signIn(req, res, next) {
-  //Validate email and password
-  const userDetail = await userModel.findOne({ email: req.body.email });
 
-  if (userDetail === null) {
-    res.send(false);
-    return;
-  }
-  const isValidPassword = encryptDecrypt.decryptPassword(
-    req.body.password,
-    userDetail.password
-  );
-  if (isValidPassword) {
-    let userData = {
-      email: userDetail.email,
-      fullName: userDetail.fullName,
-      password: userDetail.password,
-    };
-
-    //Generate JWT token and send back to frontend
-    let JWTtoken = JWTService.generateToken(userData);
-    //Insert token in DB
-    await tokenModel.insertMany([{ userId: userDetail._id, token: JWTtoken }]);
-
-    response.success(res, {
-      token: JWTtoken,
-      userDetail: userDetail,
-    });
-  } else {
-    res.json({ message: "password is invalid" });
-  }
-}
-*/
 const signIn = (req, res) => {
   userModel.findOne({ email: req.body.email }, (err, data) => {
     console.log(err);
