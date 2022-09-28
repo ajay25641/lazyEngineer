8c45c19ffe220a9b65f136686c508d5953700a2f
ajay25641 <ajay25641@gmail.com>
2022-09-28 10:57:25 +0530

    g

--- Controllers/auth.controller.js
+++ Controllers/auth.controller.js
@@ -69,10 +69,11 @@ async function signIn(req, res, next) {
   //  console.log(response);
     const token= JWTService.generateToken();
     console.log(token);
+    res.status(200).json(response);
 }).catch(err=>{
    res.status(500).json(err);
 })
-  res.status(200).json(response);
+  
  }
 
 
